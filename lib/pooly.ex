05994a004796e46360a53705302f9a81f3f8bf8d
Zia Zek <ziazek@gmail.com>
2017-05-12 12:05:39 +0800

    version 1 complete

--- lib/pooly.ex
+++ lib/pooly.ex
@@ -1,18 +1,26 @@
 defmodule Pooly do
-  @moduledoc """
-  Documentation for Pooly.
-  """
+  use Application 
+  alias Pooly.Supervisor
+  alias Pooly.Server
 
-  @doc """
-  Hello world.
+  def start(_type, _args) do
+    pool_config = [mfa: {SampleWorker, :start_link, []}, size: 5]
+    start_pool(pool_config)
+  end
 
-  ## Examples
+  def start_pool(pool_config) do
+    Supervisor.start_link(pool_config)
+  end
 
-      iex> Pooly.hello
-      :world
+  def checkout do
+    Server.checkout
+  end
 
-  """
-  def hello do
-    :world
+  def checkin(worker_pid) do
+    Server.checkin(worker_pid)
+  end
+
+  def status do
+    Server.status
   end
 end
